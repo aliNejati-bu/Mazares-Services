0c22aabdff4531c46f54aaa31bad0467ea5047bc
aliNejati-bu <electrocellco@gmail.com>
2022-06-02 17:18:27 +0430

    add game model.

---
+++ App/Model/Game.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace RemoteConfig\App\Model;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Game extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'packagename',
+        'game_name'
+    ];
+
+    /**
+     * @return BelongsToMany
+     */
+    public function users(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
